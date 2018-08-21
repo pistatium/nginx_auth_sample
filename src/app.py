1271bbc26afeb2e2db41bbe2e3fea13ed360c122
Kimihiro N <kimihiro.nisimura@gmail.com>
2018-08-21 12:31:29 +0900

    :+1: Create pages

---
+++ src/app.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from flask import Flask, request, make_response, redirect, abort
+
+app = Flask(__name__)
+
+SESSION_KEY = 'SESSION'
+
+
+@app.route('/auth/login')
+def login():
+    response = make_response(redirect('/'))
+    response.set_cookie(SESSION_KEY, '1')
+    return response 
+
+
+@app.route('/auth/logout')
+def logout():
+    response = make_response(redirect('/'))
+    response.set_cookie(SESSION_KEY, '', expires=0)  # Delete cookie 
+    return response 
+
+
+@app.route('/auth/is_login')
+def is_login():
+    if not request.cookies.get(SESSION_KEY):
+        abort(401)
+    return ''  # 200
+
